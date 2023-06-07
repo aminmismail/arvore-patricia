789a39625b7ef820d4dc27a6f01c62836480cfaa
nimA <aminismail2002@gmail.com>
2023-06-06 23:46:41 -0300

    estrutura patricia

--- patricia.h
+++ patricia.h
@@ -1,6 +1,13 @@
 #ifndef PATRICIATESTE_PATRICIA_H
 #define PATRICIATESTE_PATRICIA_H
 
+#include <stdio.h>
+#include <stdlib.h>
 
+typedef struct no{
+    char* texto;
+    struct no* filhos[1000];
+    int numFilhos;
+}No;
 
 #endif //PATRICIATESTE_PATRICIA_H
